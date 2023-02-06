6186ba048f535055a77f4d293334b06b5ae11353
KoderDhruv <dhruvbanuni@gmail.com>
2023-02-06 18:14:18 +0530

    Learning_DSA

--- bit_manipulation.java
+++ bit_manipulation.java
@@ -1,5 +1,20 @@
 public class bit_manipulation {
 
+  public static int countSetBits(int n) {
+    int count = 0;
+    while (n != 0) {
+      if ((n & 1) == 1) {
+        count++;
+      }
+      n = n >> 1;
+    }
+    return count;
+  }
+
+  public static boolean isPowerOfTwo(int n) {
+    return ((n & (n - 1)) == 0);
+  }
+
   public static int clearBits(int n, int i, int j) {
     int starting_mask = ((~0) << (j + 1));
     int ending_mask = (1 << i) - 1;
@@ -37,6 +52,8 @@ public class bit_manipulation {
   }
 
   public static void main(String args[]) {
-    System.out.println(clearBits(31,1,3));
+    System.out.println(clearBits(31, 1, 3));
+    System.out.println(isPowerOfTwo(32));
+    System.out.println(countSetBits(16));
   }
 }
