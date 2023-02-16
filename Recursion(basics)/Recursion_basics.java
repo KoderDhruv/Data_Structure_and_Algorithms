34260ae546239094cd7a550039399d87978e2725
KoderDhruv <dhruvbanuni@gmail.com>
2023-02-16 17:30:21 +0530

    Learning_DSA

--- Recursion(basics)/Recursion_basics.java
+++ Recursion(basics)/Recursion_basics.java
@@ -1,5 +1,44 @@
 public class Recursion_basics {
 
+  public static int powerOptimized(int x, int n) {
+    if (n == 0) {
+      return 1;
+    }
+    int halfPower = powerOptimized(x, n / 2);
+    if (n % 2 != 0) {
+      return (x * halfPower * halfPower);
+    } else {
+      return (halfPower * halfPower);
+    }
+  }
+
+  public static int power(int x, int n) {
+    if (n == 0) {
+      return 1;
+    }
+    return x * power(x, n - 1);
+  }
+
+  public static int lastOccurence(int arr[], int key, int i) {
+    if (i == arr.length - 1) {
+      return -1;
+    }
+    if ((arr[i] == key) && (lastOccurence(arr, key, i + 1) == -1)) {
+      return i;
+    }
+    return lastOccurence(arr, key, i + 1);
+  }
+
+  public static int firstOccurence(int arr[], int key, int i) {
+    if (i == arr.length - 1) {
+      return -1;
+    }
+    if (arr[i] == key) {
+      return i;
+    }
+    return firstOccurence(arr, key, i + 1);
+  }
+
   public static boolean isSorted(int arr[], int i) {
     if (i == arr.length - 1) {
       return true;
@@ -33,25 +72,28 @@ public class Recursion_basics {
 
   public static void printDecreasing(int n) {
     if (n == 1) {
-      System.out.print(n + " ");
+      System.out.println(n + " ");
       return;
     }
-    System.out.print(n + " ");
+    System.out.println(n + " ");
     printDecreasing(n - 1);
   }
 
   public static void printIncreasing(int n) {
     if (n == 1) {
-      System.out.print(n + " ");
+      System.out.println(n + " ");
       return;
     }
     printIncreasing(n - 1);
-    System.out.print(n + " ");
+    System.out.println(n + " ");
   }
 
   public static void main(String args[]) {
     int n = 6;
-    int arr[]={1,2,9,5,6};
+    int arr[] = { 1, 9, 9, 5, 6 };
+    printDecreasing(n);
     System.out.println(isSorted(arr, 0));
+    System.out.println(lastOccurence(arr, 9, 0));
+    System.out.println(powerOptimized(2, 5));
   }
 }
