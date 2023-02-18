292d93bf69b070f5baf44e16e0ffb42189b9cff4
KoderDhruv <dhruvbanuni@gmail.com>
2023-02-18 18:43:21 +0530

    Learning_DSA

--- Recursion_basics/Recursion_basics.java
+++ Recursion_basics/Recursion_basics.java
@@ -1,5 +1,36 @@
 public class Recursion_basics {
 
+  public static void binaryString(){
+    
+  }
+
+  public static int friendsPairing(int n) {
+    if (n == 1 || n == 2) {
+      return n;
+    }
+    return (friendsPairing(n - 1) + ((n - 1) * friendsPairing(n - 2)));
+  }
+
+  public static void removeDuplicate(String str, int idx, StringBuilder newStr, boolean map[]) {
+    if (idx == str.length()) {
+      System.out.println(newStr);
+      return;
+    }
+    if (map[str.charAt(idx) - 'a'] == true) {
+      removeDuplicate(str, idx + 1, newStr, map);
+    } else {
+      map[str.charAt(idx) - 'a'] = true;
+      removeDuplicate(str, idx + 1, newStr.append(str.charAt(idx)), map);
+    }
+  }
+
+  public static int tilingProblem(int n) {
+    if (n == 0 || n == 1) {
+      return 1;
+    }
+    return tilingProblem(n - 1) + tilingProblem(n - 2);
+  }
+
   public static int powerOptimized(int x, int n) {
     if (n == 0) {
       return 1;
@@ -50,8 +81,8 @@ public class Recursion_basics {
   }
 
   public static int fib(int n) {
-    if (n == 1 || n == 2) {
-      return 1;
+    if (n == 0 || n == 1) {
+      return n;
     }
     return (fib(n - 1) + fib(n - 2));
   }
@@ -91,9 +122,12 @@ public class Recursion_basics {
   public static void main(String args[]) {
     int n = 6;
     int arr[] = { 1, 9, 9, 5, 6 };
-    printDecreasing(n);
+    String str = "dhruvbanuni";
+    System.out.println(fib(n));
     System.out.println(isSorted(arr, 0));
     System.out.println(lastOccurence(arr, 9, 0));
     System.out.println(powerOptimized(2, 5));
+    removeDuplicate(str, 0, new StringBuilder(""), new boolean[26]);
+    System.out.println(friendsPairing(4));
   }
 }
