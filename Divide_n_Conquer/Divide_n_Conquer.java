90ed4920d1209fb6f2e0258c4f70c00dc9d69cd3
KoderDhruv <dhruvbanuni@gmail.com>
2023-02-19 14:09:05 +0530

    Learning_DSA

--- Divide_n_Conquer/Divide_n_Conquer.java
+++ Divide_n_Conquer/Divide_n_Conquer.java
@@ -1,7 +1,14 @@
-package Divide_n_Conquer;
 
 public class Divide_n_Conquer {
+
+  public static void printArr(int arr[]) {
+    for (int i = 0; i < arr.length; i++) {
+      System.out.print(arr[i] + " ");
+    }
+  }
+
   public static void main(String args[]) {
-    
+    int arr[] = { 6, 3, 9, 5, 2, 8 };
+    printArr(arr);
   }
 }
