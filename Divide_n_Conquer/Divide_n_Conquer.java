fa9fd36574bd7b817dc7d38854918c8a88d39c5a
KoderDhruv <dhruvbanuni@gmail.com>
2023-02-22 09:25:29 +0530

    Learning_DSA

--- Divide_n_Conquer/Divide_n_Conquer.java
+++ Divide_n_Conquer/Divide_n_Conquer.java
@@ -1,8 +1,39 @@
 
 public class Divide_n_Conquer {
 
-  public static void quickSort(int arr[], int si, int ei) {
+  public static int searchRotatedArr(int arr[], int key, int si, int ei) {
 
+    if (si > ei) {
+      return -1;
+    }
+
+    int mid = si + (ei - si) / 2;
+
+    if (arr[mid] == key) {
+      return mid;
+    }
+
+    // mid on line_1
+    if (arr[si] <= arr[mid]) {
+      if (arr[si] <= key && key <= arr[mid]) {
+        return searchRotatedArr(arr, key, si, mid - 1);
+      } else {
+        return searchRotatedArr(arr, key, mid + 1, ei);
+      }
+    }
+
+    // mid on line_2
+    else {
+      if (arr[mid] <= key && key <= arr[ei]) {
+        return searchRotatedArr(arr, key, mid + 1, ei);
+      } else {
+        return searchRotatedArr(arr, key, si, mid - 1);
+      }
+    }
+
+  }
+
+  public static void quickSort(int arr[], int si, int ei) {
     if (si >= ei) {
       return;
     }
@@ -14,7 +45,11 @@ public class Divide_n_Conquer {
 
   public static int partition(int arr[], int si, int ei) {
     int pivot = arr[ei];
-    int i = si - 1; // make place for elements less than pivot
+    int i = si - 1;
+    // 'i' does not change when arr[j] is larger than pivot and then swaps when
+    // arr[j] is smaller, so smaller elements comes first in the array.
+    // We can alternatively, run two pointers in a loop which swaps under certain
+    // condition making smaller elements come first and larger after the pivot.
     for (int j = si; j < ei; j++) {
       if (arr[j] <= pivot) {
         i++;
@@ -24,6 +59,7 @@ public class Divide_n_Conquer {
         arr[i] = temp;
       }
     }
+    // swap with pivot
     i++;
     int temp = pivot;
     arr[ei] = arr[i];
@@ -84,6 +120,8 @@ public class Divide_n_Conquer {
 
   public static void main(String args[]) {
     int arr[] = { 6, 3, 9, 5, 2, 8 };
+    int rotatedArr[] = { 4, 5, 6, 7, 0, 1, 2 };
+    System.out.println(searchRotatedArr(rotatedArr, 5, 0, arr.length-1));
     quickSort(arr, 0, arr.length - 1);
     printArr(arr);
   }
