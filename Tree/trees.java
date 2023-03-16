327ddd0890f375483002d0ab00cd2225428ff1d7
KoderDhruv <dhruvbanuni@gmail.com>
2023-03-17 01:44:25 +0530

    Learning_DSA

--- Tree/trees.java
+++ Tree/trees.java
@@ -173,6 +173,97 @@ public class trees {
     return isSubtree(root.left, subroot) || isSubtree(root.right, subroot);
   }
 
+  static class Info {
+    Node node;
+    int hd;
+
+    public Info(Node node, int hd) {
+      this.node = node;
+      this.hd = hd;
+    }
+  }
+
+  public static void topView(Node root) {
+    Queue<Info> q = new LinkedList<>();
+    HashMap<Integer, Node> map = new HashMap<>();
+    int min = 0, max = 0;
+    q.add(new Info(root, 0));
+    q.add(null);
+    while (!q.isEmpty()) {
+      Info curr = q.remove();
+      if (curr == null) {
+        if (q.isEmpty()) {
+          break;
+        } else {
+          q.add(null);
+        }
+      } else {
+        if (!map.containsKey(curr.hd)) {
+          map.put(curr.hd, curr.node);
+        }
+        if (curr.node.left != null) {
+          q.add(new Info(curr.node.left, curr.hd - 1));
+          min = Math.min(min, curr.hd - 1);
+        }
+        if (curr.node.right != null) {
+          q.add(new Info(curr.node.right, curr.hd + 1));
+          max = Math.max(max, curr.hd + 1);
+        }
+      }
+    }
+    for (int i = min; i <= max; i++) {
+      System.out.print(map.get(i).data + " ");
+    }
+    System.out.println();
+  }
+
+  public static void KLevel(Node root, int k, int level) {
+    if (root == null) {
+      return;
+    }
+    if (level == k) {
+      System.out.print(root.data + " ");
+      return;
+    }
+    KLevel(root.left, k, level + 1);
+    KLevel(root.right, k, level + 1);
+  }
+
+  public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+    if (root == null) {
+      return false;
+    }
+    path.add(root);
+    if (root.data == n) {
+      return true;
+    }
+    boolean foundLeft = getPath(root.left, n, path);
+    boolean foundRight = getPath(root.right, n, path);
+    if (foundLeft || foundRight) {
+      return true;
+    }
+    path.remove(path.size() - 1);
+    return false;
+  }
+
+  public static Node lca(Node root, int n1, int n2) {
+    ArrayList<Node> path1 = new ArrayList<>();
+    ArrayList<Node> path2 = new ArrayList<>();
+    getPath(root, n1, path1);
+    getPath(root, n2, path2);
+    int i = 0;
+    for (; i < path1.size() && i < path2.size(); i++) {
+      if (path1.get(i) != path2.get(i)) {
+        break;
+      }
+    }
+    return path1.get(i - 1);
+  }
+
+  public static Node lca_optimized(Node root, int n1, int n2){
+    
+  }
+
   public static void main(String args[]) {
     // int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
     // BinaryTree tree = new BinaryTree();
@@ -186,14 +277,47 @@ public class trees {
     // System.out.println(tree.diameter(root));
     // System.out.println(tree.diameter_optimized(root).diam);
 
-    // subtree of another tree
-    /*
-                       1                    2
-                      /  \                 / \
-                     2    3                4   5
-                    / \  / \
-                   4   5 6  7
-     */
+    // Subtree of another tree
+
+    // Node root = new Node(1);
+    // root.left = new Node(2);
+    // root.right = new Node(3);
+    // root.left.left = new Node(4);
+    // root.left.right = new Node(5);
+    // root.right.left = new Node(6);
+    // root.right.right = new Node(7);
+
+    // Node subroot = new Node(2);
+    // subroot.left = new Node(4);
+    // subroot.right = new Node(5);
+
+    // System.out.println(isSubtree(root, subroot));
+
+    // Top view of a tree
+
+    // Node root = new Node(1);
+    // root.left = new Node(2);
+    // root.right = new Node(3);
+    // root.left.left = new Node(4);
+    // root.left.right = new Node(5);
+    // root.right.left = new Node(6);
+    // root.right.right = new Node(7);
+    // topView(root);
+
+    // Elements in Kth level of tree
+
+    // Node root = new Node(1);
+    // root.left = new Node(2);
+    // root.right = new Node(3);
+    // root.left.left = new Node(4);
+    // root.left.right = new Node(5);
+    // root.right.left = new Node(6);
+    // root.right.right = new Node(7);
+    // int k = 3;
+    // KLevel(root, k, 1);
+
+    // Lowest Common Ancestor
+    int n1=4, n2=5;
     Node root = new Node(1);
     root.left = new Node(2);
     root.right = new Node(3);
@@ -201,11 +325,7 @@ public class trees {
     root.left.right = new Node(5);
     root.right.left = new Node(6);
     root.right.right = new Node(7);
-
-    Node subroot = new Node(2);
-    subroot.left = new Node(4);
-    subroot.right = new Node(5);
-
-    System.out.println(isSubtree(root, subroot));
+    System.out.println(lca(root, n1, n2).data);
+    System.out.println(lca_optimized(root, n1, n2).data);
   }
 }
