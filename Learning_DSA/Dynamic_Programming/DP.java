54c34533726989a5131aab6c3e813e192cdf982e
KoderDhruv <dhruvbanuni@gmail.com>
2023-06-26 18:23:02 +0530

    Learning_DSA

--- Learning_DSA/Dynamic_Programming/DP.java
+++ Learning_DSA/Dynamic_Programming/DP.java
@@ -1,13 +1,90 @@
-public class DP {
 
+public class DP {
+  // Target Sum Subset
+  public static void targetSum(int arr[], int sum) {
+    int n = arr.length;
+    boolean[][] dp = new boolean[n + 1][sum + 1];
+    for (int i = 0; i < n + 1; i++)
+      dp[i][0] = true;
+
+  }
+
+  // 0-1 Knapsack
+  public static int knapsack(int val[], int wt[], int W, int n) {
+    if (W == 0 || n == 0)
+      return 0;
+    if (wt[n - 1] <= W) { // ifValid
+      // include
+      int ans1 = val[n - 1] + knapsack(val, wt, W - wt[n - 1], n - 1);
+      // exclude
+      int ans2 = knapsack(val, wt, W, n - 1);
+      return Math.max(ans1, ans2);
+    } else { // not Valid, so exclude
+      return knapsack(val, wt, W, n - 1);
+    }
+  }
+
+  public static int knapsackMemoization(int[] val, int[] wt, int W, int n) {
+    int dp[][] = new int[val.length + 1][W + 1];
+    for (int i = 0; i < val.length + 1; i++) {
+      for (int j = 0; j < W + 1; j++) {
+        dp[i][j] = -1;
+      }
+    }
+    if (W == 0 || n == 0)
+      return 0;
+    if (dp[n][W] != -1)
+      return dp[n][W];
+    if (wt[n - 1] <= W) { // ifValid
+      // include
+      int ans1 = val[n - 1] + knapsackMemoization(val, wt, W - wt[n - 1], n - 1);
+      // exclude
+      int ans2 = knapsackMemoization(val, wt, W, n - 1);
+      dp[n][W] = Math.max(ans1, ans2);
+      return dp[n][W];
+    } else { // not Valid, so exclude
+      dp[n][W] = knapsackMemoization(val, wt, W, n - 1);
+      return dp[n][W];
+    }
+  }
+
+  public static int knapsackTabulation(int[] val, int[] wt, int W, int n) {
+    int dp[][] = new int[val.length + 1][W + 1];
+    for (int i = 0; i < dp.length; i++)
+      dp[i][0] = 0;
+    for (int i = 0; i < dp[0].length; i++)
+      dp[0][i] = 0;
+    for (int i = 1; i < n + 1; i++) { // "i" is number of items
+      for (int j = 1; j < W + 1; j++) { // "j" is maximum weight
+        int v = val[i - 1];
+        int w = wt[i - 1];
+        if (w <= j) { // ifValid
+          int incProfit = v + dp[i - 1][j - w]; // include
+          int excProfit = dp[i - 1][j]; // exclude
+          dp[i][j] = Math.max(incProfit, excProfit);
+        } else { // if not Valid, so exclude
+          int excProfit = dp[i - 1][j];
+          dp[i][j] = excProfit;
+        }
+      }
+    }
+    // Printing dp array
+    for (int i = 0; i < dp.length; i++) {
+      for (int j = 0; j < dp[0].length; j++) {
+        System.out.print(dp[i][j] + " ");
+      }
+      System.out.println();
+    }
+    return dp[n][W];
+  }
+
+  // Fibonacci series
   public static int fibMemoization(int n) {
     int f[] = new int[n + 1];
-    if (n == 0 || n == 1) {
+    if (n == 0 || n == 1)
       return n;
-    }
-    if (f[n] != 0) {
+    if (f[n] != 0)
       return f[n];
-    }
     f[n] = fibMemoization(n - 1) + fibMemoization(n - 2);
     return f[n];
   }
@@ -21,51 +98,6 @@ public class DP {
     return dp[n];
   }
 
-  public static int knapsack(int val[], int wt[], int W, int n) {
-    if (W == 0 || n == 0) {
-      return 0;
-    }
-    if (wt[n - 1] <= W) { // ifValid
-      // include
-      int ans1 = val[n - 1] + knapsack(val, wt, W - wt[n - 1], n - 1);
-      // exclude
-      int ans2 = knapsack(val, wt, W, n - 1);
-      return Math.max(ans1, ans2);
-    } else {
-      return knapsack(val, wt, W, n - 1);
-    }
-  }
-
-  public static int knapsackMemoization(int[] val, int[] wt, int W, int n) {
-    int dp[][] = new int[val.length + 1][W + 1];
-    for (int i = 0; i < val.length + 1; i++) {
-      for (int j = 0; j < W + 1; j++) {
-        dp[i][j] = -1;
-      }
-    }
-    if (W == 0 || n == 0) {
-      return 0;
-    }
-    if (dp[n][W] != -1) {
-      return dp[n][W];
-    }
-    if (wt[n - 1] <= W) { // ifValid
-      // include
-      int ans1 = val[n - 1] + knapsackMemoization(val, wt, W - wt[n - 1], n - 1);
-      // exclude
-      int ans2 = knapsackMemoization(val, wt, W, n - 1);
-      dp[n][W] = Math.max(ans1, ans2);
-      return dp[n][W];
-    } else {
-      dp[n][W] = knapsackMemoization(val, wt, W, n - 1);
-      return dp[n][W];
-    }
-  }
-
-  public static int knapsackTabulation(){
-    return 0;
-  }
-
   public static void main(String args[]) {
     // Fibonacci
     // int n = 5;
@@ -73,11 +105,16 @@ public class DP {
     // System.out.println(fibTabulation(n));
 
     // 0-1 Knapsack
-    int val[] = { 15, 14, 10, 45, 30 };
-    int wt[] = { 2, 5, 1, 3, 4 };
-    int W = 7;
-    System.out.println(knapsack(val, wt, W, val.length));
-    System.out.println(knapsackMemoization(val, wt, W, val.length));
-    System.out.println();
+    // int val[] = { 15, 14, 10, 45, 30 };
+    // int wt[] = { 2, 5, 1, 3, 4 };
+    // int W = 7;
+    // System.out.println(knapsack(val, wt, W, val.length));
+    // System.out.println(knapsackMemoization(val, wt, W, val.length));
+    // System.out.println(knapsackTabulation(val, wt, W, val.length));
+
+    // Target-Sum
+    int arr[] = { 4, 2, 1, 7, 1, 3 };
+    int sum = 10;
+    targetSum(arr, sum);
   }
 }
--- Learning_DSA/Graph/graphs.java
+++ Learning_DSA/Graph/graphs.java
@@ -13,12 +13,191 @@ public class graphs {
     }
   }
 
+  static class Pair implements Comparable<Pair> {
+    int n;
+    int path;
+
+    Pair(int n, int path) {
+      this.n = n;
+      this.path = path;
+    }
+
+    @Override
+    public int compareTo(Pair p2) {
+      return this.path - p2.path;
+    }
+  }
+
+  // Dijkstra's Algorithm
+  public static void Dijkstra(ArrayList<Edge> graph[], int src) {
+    int dist[] = new int[graph.length];
+    for (int i = 0; i < graph.length; i++) {
+      if (i != src) {
+        dist[i] = Integer.MAX_VALUE;
+      }
+    }
+    boolean vis[] = new boolean[graph.length];
+    PriorityQueue<Pair> pq = new PriorityQueue<>();
+    pq.add(new Pair(src, 0));
+    while (!pq.isEmpty()) {
+      Pair curr = pq.remove();
+      if (!vis[curr.n]) {
+        vis[curr.n] = true;
+        // neighbours
+        for (int i = 0; i < graph[curr.n].size(); i++) {
+          Edge e = graph[curr.n].get(i);
+          int u = e.src;
+          int v = e.dest;
+          int wt = e.wt;
+          if (dist[u] + wt < dist[v]) {
+            dist[v] = dist[u] + wt;
+            pq.add(new Pair(v, dist[v]));
+          }
+        }
+      }
+    }
+    for (int i = 0; i < dist.length; i++) {
+      System.out.print(dist[i] + " ");
+    }
+    System.out.println();
+  }
+
+  // All path from source to destination
+  public static void printAllPath(ArrayList<Edge> graph[], int src, int dest, String path) {
+    if (src == dest) {
+      System.out.println(path + dest);
+      return;
+    }
+    for (int i = 0; i < graph.length; i++) {
+      Edge e = graph[src].get(i);
+      printAllPath(graph, e.dest, dest, path + src);
+    }
+  }
+
+  // Topological sort using BFS (Kahn's Algorithm)
+  public static void kahn(ArrayList<Edge> graph[]) {
+    int indeg[] = new int[graph.length];
+    calcIndeg(graph, indeg);
+    Queue<Integer> q = new LinkedList<>();
+    for (int i = 0; i < indeg.length; i++) {
+      if (indeg[i] == 0) {
+        q.add(i);
+      }
+    }
+    while (!q.isEmpty()) {
+      int curr = q.remove();
+      System.out.print(curr + " ");
+      for (int i = 0; i < graph[curr].size(); i++) {
+        Edge e = graph[curr].get(i);
+        indeg[e.dest]--;
+        if (indeg[e.dest] == 0) {
+          q.add(e.dest);
+        }
+      }
+    }
+    System.out.println();
+  }
+
+  public static void calcIndeg(ArrayList<Edge> graph[], int indeg[]) {
+    for (int i = 0; i < graph.length; i++) {
+      int v = i;
+      for (int j = 0; j < graph[v].size(); j++) {
+        Edge e = graph[v].get(j);
+        indeg[e.dest]++;
+      }
+    }
+  }
+
+  // Topological Sort (Only for Directed Acyclic Graph)
+  public static void topologicalSort(ArrayList<Edge> graph[]) {
+    boolean vis[] = new boolean[graph.length];
+    Stack<Integer> s = new Stack<>();
+    for (int i = 0; i < graph.length; i++) {
+      if (!vis[i]) {
+        topologicalSortUtil(graph, i, vis, s);
+      }
+    }
+    while (!s.isEmpty()) {
+      System.out.print(s.pop() + " ");
+    }
+  }
+
+  public static void topologicalSortUtil(ArrayList<Edge> graph[], int curr, boolean vis[], Stack<Integer> s) {
+    vis[curr] = true;
+    for (int i = 0; i < graph.length; i++) {
+      Edge e = graph[curr].get(i);
+      if (!vis[i]) {
+        topologicalSortUtil(graph, e.dest, vis, s);
+      }
+    }
+    s.push(curr);
+  }
+
+  // Cycle detection in direcetd graph
+  public static boolean isCycleDirected(ArrayList<Edge> graph[]) {
+    boolean vis[] = new boolean[graph.length];
+    boolean stack[] = new boolean[graph.length];
+    for (int i = 0; i < graph.length; i++) {
+      if (!vis[i]) {
+        if (isCycleDirectedUtil(graph, i, vis, stack)) {
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  public static boolean isCycleDirectedUtil(ArrayList<Edge> graph[], int curr, boolean vis[], boolean[] stack) {
+    vis[curr] = true;
+    stack[curr] = true;
+    for (int i = 0; i < graph[curr].size(); i++) {
+      Edge e = graph[curr].get(i);
+      if (stack[e.dest]) {
+        return true;
+      }
+      if (!vis[e.dest] && isCycleDirectedUtil(graph, e.dest, vis, stack)) {
+        return true;
+      }
+    }
+    stack[curr] = false;
+    return false;
+  }
+
+  // Check if graph is bipartite
+  public static boolean isBipartite(ArrayList<Edge>[] graph) {
+    int col[] = new int[graph.length];
+    for (int i = 0; i < graph.length; i++) {
+      col[i] = -1;
+    }
+    Queue<Integer> q = new LinkedList<>();
+    for (int i = 0; i < graph.length; i++) {
+      if (col[i] == -1) {
+        q.add(i);
+        col[i] = 0;
+        while (!q.isEmpty()) {
+          int curr = q.remove();
+          for (int j = 0; j < graph[curr].size(); j++) {
+            Edge e = graph[curr].get(j);
+            if (col[e.dest] == -1) {
+              int nextCol = col[curr] == 0 ? 1 : 0;
+              col[e.dest] = nextCol;
+              q.add(e.dest);
+            } else if (col[e.dest] == col[curr]) {
+              return false;
+            }
+          }
+        }
+      }
+    }
+    return true;
+  }
+
   // Detect cycle in a graph
   public static boolean cycleDetect(ArrayList<Edge>[] graph) {
     boolean[] vis = new boolean[graph.length];
     for (int i = 0; i < graph.length; i++) {
       if (!vis[i]) {
-        if(cycleDetectUtil(graph, vis, i, -1)){
+        if (cycleDetectUtil(graph, vis, i, -1)) {
           return true;
         }
       }
@@ -27,23 +206,33 @@ public class graphs {
   }
 
   public static boolean cycleDetectUtil(ArrayList<Edge>[] graph, boolean[] vis, int curr, int par) {
-    
+    vis[curr] = true;
+    for (int i = 0; i < graph[curr].size(); i++) {
+      Edge e = graph[curr].get(i);
+      if (!vis[e.dest]) {
+        if (cycleDetectUtil(graph, vis, e.dest, curr)) {
+          return true;
+        }
+      } else if (vis[e.dest] && e.dest != par) {
+        return true;
+      }
+    }
     return false;
   }
 
   // BFS for disjoint components in a garph
   public static void bfsConnect(ArrayList<Edge>[] graph) {
     boolean vis[] = new boolean[graph.length];
+    Queue<Integer> q = new LinkedList<>();
     for (int i = 0; i < graph.length; i++) {
       if (!vis[i]) {
-        bfsUtil(graph, vis);
+        bfsUtil(graph, vis, i, q);
       }
     }
   }
 
-  public static void bfsUtil(ArrayList<Edge>[] graph, boolean[] vis) {
-    Queue<Integer> q = new LinkedList<>();
-    q.add(0);
+  public static void bfsUtil(ArrayList<Edge>[] graph, boolean[] vis, int src, Queue<Integer> q) {
+    q.add(src);
     while (!q.isEmpty()) {
       int curr = q.remove();
       if (!vis[curr]) {
@@ -120,28 +309,42 @@ public class graphs {
       graph[i] = new ArrayList<Edge>();
     }
 
-    graph[0].add(new Edge(0, 1, 1));
-    graph[0].add(new Edge(0, 2, 1));
+    // undirected graph
+    // graph[0].add(new Edge(0, 1, 1));
+    // graph[0].add(new Edge(0, 2, 1));
 
-    graph[1].add(new Edge(1, 0, 1));
-    graph[1].add(new Edge(1, 3, 1));
+    // graph[1].add(new Edge(1, 0, 1));
+    // graph[1].add(new Edge(1, 3, 1));
 
-    graph[2].add(new Edge(2, 0, 1));
-    graph[2].add(new Edge(2, 4, 1));
+    // graph[2].add(new Edge(2, 0, 1));
+    // graph[2].add(new Edge(2, 4, 1));
+
+    // graph[3].add(new Edge(3, 1, 1));
+    // graph[3].add(new Edge(3, 4, 1));
+    // graph[3].add(new Edge(3, 5, 1));
+
+    // graph[4].add(new Edge(4, 2, 1));
+    // graph[4].add(new Edge(4, 3, 1));
+    // graph[4].add(new Edge(4, 5, 1));
+
+    // graph[5].add(new Edge(5, 3, 1));
+    // graph[5].add(new Edge(5, 4, 1));
+    // graph[5].add(new Edge(5, 6, 1));
+
+    // graph[6].add(new Edge(6, 5, 1));
+
+    // directed acyclic graph
+    graph[0].add(new Edge(0, 3, 1));
+
+    graph[2].add(new Edge(2, 3, 1));
 
     graph[3].add(new Edge(3, 1, 1));
-    graph[3].add(new Edge(3, 4, 1));
-    graph[3].add(new Edge(3, 5, 1));
 
-    graph[4].add(new Edge(4, 2, 1));
-    graph[4].add(new Edge(4, 3, 1));
-    graph[4].add(new Edge(4, 5, 1));
+    graph[4].add(new Edge(4, 0, 1));
+    graph[4].add(new Edge(4, 1, 1));
 
-    graph[5].add(new Edge(5, 3, 1));
-    graph[5].add(new Edge(5, 4, 1));
-    graph[5].add(new Edge(5, 6, 1));
-
-    graph[6].add(new Edge(6, 5, 1));
+    graph[5].add(new Edge(5, 0, 1));
+    graph[5].add(new Edge(5, 2, 1));
   }
 
   public static void main(String args[]) {
@@ -158,5 +361,10 @@ public class graphs {
     // bfs(graph);
     // dfs(graph, 0, new boolean[7]);
 
+    // Detect cycle
+    // System.out.print(cycleDetect(graph));
+
+    // isBipartite
+    // System.out.print(isBipartite(graph));
   }
 }
